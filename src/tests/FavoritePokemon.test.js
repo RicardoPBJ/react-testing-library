50199a1fcf2432fe6efb2e0674671c1aa1dc36bf
RicardoPBJ <rbrumjunio@gmail.com>
2022-11-26 04:14:41 -0300

    Teste o componente <FavoritePokemon.js />

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Ao renderizar a pagina FavoritePokemon...', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos.', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const notFoundText = screen.getByText(/no favorite pokémon found/i);
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
