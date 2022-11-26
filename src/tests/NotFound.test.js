fe96c5af2cfc7d0c8878e1ea6b795313401b849a
RicardoPBJ <rbrumjunio@gmail.com>
2022-11-26 04:30:07 -0300

    Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Ao renderizar o componente NotFound... ', () => {
+  test('', () => {
+    renderWithRouter(<NotFound />);
+
+    const h2Element = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(h2Element).toBeInTheDocument();
+  });
+
+  test('', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundImg = screen.getByRole('img', {
+      src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    });
+
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg.getAttribute('src')).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
